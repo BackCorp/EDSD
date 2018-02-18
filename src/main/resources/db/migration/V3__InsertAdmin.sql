e18dae8ecb6a235f39beccfef8e1de47ca571dcb
Brice Djilo <dbrice@uno.edu>
2018-02-18 14:32:13 -0700

    Initial config: migration V3

---
+++ src/main/resources/db/migration/V3__InsertAdmin.sql
@@ -0,0 +1,11 @@
+INSERT INTO `edsd`.`user`
+(`last_name`,
+`first_name`,
+`username`,
+`password`,
+`email`,
+`active`)
+VALUES
+('Admin','Admin','admin','padmin','admin@edsd.com',1);
+
+INSERT INTO `edsd`.`user_role` (`user_id`,`role_id`) VALUES (1,1);
